96bc3ba15bed3af7972173d19aee49e3c6f53817
Aldis Poga <aldis.poga@gmail.com>
2023-12-16 18:36:45 +0200

    small change

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient({
-  defaultOptions: { queries: { staleTime: 10000}}
+  defaultOptions: { queries: { staleTime: Infinity}}
 })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
